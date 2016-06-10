2a3b782f3bfd875533114cebccee3da945578174
Igor Zinovyev <zinigor@gmail.com>
2016-06-10 18:43:10 +0300

    Trying to fix Node environment.

--- tests/load-travis.sh
+++ tests/load-travis.sh
@@ -5,6 +5,7 @@ if [ "$WP_TRAVISCI" != "phpunit" ]; then
 	gem install compass
 	source ~/.nvm/nvm.sh
 	nvm install node
+	nvm alias default node
 	npm install -g npm
 	npm install -g gulp-cli
 	npm install
