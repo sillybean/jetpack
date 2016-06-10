3f4157f9ffdf01d9fc7036fa3159929183bb1064
Igor Zinovyev <zinigor@gmail.com>
2016-06-10 19:45:26 +0300

    Added more verbosity.

--- tests/load-travis.sh
+++ tests/load-travis.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 if [ "$WP_TRAVISCI" != "phpunit" ]; then
-	gem install sass
-	gem install compass
-	source ~/.nvm/nvm.sh
-	nvm install node
-	nvm alias default node
-	npm install -g npm
-	npm install -g gulp-cli
-	npm install
-	npm rebuild node-sass
 fi
